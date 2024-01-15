e0224f41439e5629b84d881e3d799d3950041df1
Harrison Hank <codinghank@gmail.com>
2024-01-15 21:38:23 +0300

    Changes to be committed:

--- Time_based.py
+++ Time_based.py
@@ -4,17 +4,20 @@ import qrcode
 
 key = "keythatisveryrandomandsecure"
 
-totp = pyotp.TOTP(key)
+uri = pyotp.totp.TOTP(key).provisioning_uri(name="Hank" 
+                                            issuer_name="@CodeWithHank")
 
-print(totp.now()) 
+# totp = pyotp.TOTP(key)
 
-input_code = input("Enter 2FA code : ")
+# print(totp.now()) 
+
+# input_code = input("Enter 2FA code : ")
 
 
 
-if totp.verify(input_code):
-    print("Welcome")
-else:
-    print("Access Denied")
+# if totp.verify(input_code):
+#     print("Welcome")
+# else:
+#     print("Access Denied")
 
 # time.sleep(10)
