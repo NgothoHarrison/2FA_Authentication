34c00910d6cc7168b9e12d49d1d02801730bf930
Harrison Hank <codinghank@gmail.com>
2024-01-16 21:26:04 +0300

    modified:   Time_based.py

--- Time_based.py
+++ Time_based.py
@@ -10,16 +10,11 @@ print(totp.now())
 
 input_code = input("Enter 2FA code : ")
 
-uri = pyotp.totp.TOTP(key).provisioning_uri(name="Hank ",
-                                            issuer_name="2FA APP by @CodeWithHank")
 
-print(uri)
 
-qrcode.make(uri).save("code.png")
+if totp.verify(input_code):
+    print("Welcome")
+else:
+    print("Access Denied")
 
-# if totp.verify(input_code):
-#     print("Welcome")
-# else:
-#     print("Access Denied")
-
-# time.sleep(10)
+time.sleep(10)
